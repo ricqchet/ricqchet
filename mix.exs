26c152964f370cddd66cf6ffdb128b5ced435511
Sean Callan <seancallan@gmail.com>
2026-01-31 11:03:47 -0500

    fix(ci): add ex_unit to dialyzer plt_add_apps
    Fixes Dialyzer errors about missing ExUnit functions when analyzing
    test support files.

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Relay.MixProject do
       deps: deps(),
       dialyzer: [
         plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
-        plt_add_apps: [:mix]
+        plt_add_apps: [:mix, :ex_unit]
       ]
     ]
   end
