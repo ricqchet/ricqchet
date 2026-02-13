697ec8be409fcb2ecca2324e4a720c4caf8d9411
Sean Callan <seancallan@gmail.com>
2026-02-12 22:02:46 -0500

    feat: add channel members api endpoint

---
+++ lib/ricqchet_web/controllers/channel_members_controller.ex
@@ -0,0 +1,65 @@
+defmodule RicqchetWeb.ChannelMembersController do
+  @moduledoc """
+  Controller for channel members (presence).
+
+  Provides an endpoint for listing connected members of a presence channel.
+  """
+
+  use RicqchetWeb, :controller
+
+  alias Ricqchet.Channels
+  alias RicqchetWeb.Channels.Presence
+
+  action_fallback RicqchetWeb.FallbackController
+
+  @doc """
+  Lists members of a presence channel.
+
+      GET /v1/channels/:channel_name/members
+
+  Returns the list of connected users for a `presence-` prefixed channel.
+  Returns an error for non-presence channels.
+  """
+  def index(conn, %{"channel_name" => channel_name}) do
+    application = conn.assigns.current_application
+
+    with :ok <- check_channels_enabled(application),
+         :ok <- validate_channel(channel_name),
+         :ok <- check_presence_channel(channel_name) do
+      topic = "channels:app:#{application.id}:#{channel_name}"
+      presence_list = Presence.list(topic)
+      render(conn, :index, members: format_members(presence_list))
+    end
+  end
+
+  defp check_channels_enabled(application) do
+    if application.channels_enabled,
+      do: :ok,
+      else: {:error, :forbidden}
+  end
+
+  defp validate_channel(channel_name) do
+    case Channels.validate_channel_name(channel_name) do
+      :ok -> :ok
+      {:error, reason} -> {:error, :validation, reason}
+    end
+  end
+
+  defp check_presence_channel("presence-" <> _), do: :ok
+
+  defp check_presence_channel(_) do
+    {:error, :validation, "members are only available for presence channels"}
+  end
+
+  defp format_members(presence_map) do
+    Enum.map(presence_map, fn {user_id, %{metas: metas}} ->
+      meta = List.first(metas, %{})
+
+      %{
+        user_id: user_id,
+        user_info: Map.get(meta, :user_info, %{}),
+        joined_at: Map.get(meta, :joined_at)
+      }
+    end)
+  end
+end
